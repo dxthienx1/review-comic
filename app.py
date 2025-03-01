4ad257f728ae4aebb07f97e5ad7cdf72815c01c9
dxthienx1 <dxthienx1@gmail.com>
2025-03-01 17:07:01 +0700

    01-3-25

--- app.py
+++ app.py
@@ -287,7 +287,7 @@ class MainApp:
                                     next_xpath = get_xpath_by_multi_attribute('a', ['id="next_chap"'])
                                     next_ele = get_element_by_xpath(driver, next_xpath)
                                     if next_ele:
-                                        scroll_into_view(next_ele)
+                                        scroll_into_view(driver, next_ele)
                                         next_ele.click()
                                         sleep(3)
 
